02e4c046fb74b173e004bd4cab9126720fbc4aae
LoureiroManuel <loureiromanuel@itis-molinari.eu>
2025-02-16 15:39:57 +0100

    server

--- server.js
+++ server.js
@@ -1,99 +1,47 @@
-//libreria express
-const express = require("express");
-const app = express();
-const bodyParser = require('body-parser');
-const http = require("http");
-const multer = require('multer');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-   extended: true
-}));
-
+const express = require("express");//varie librerie che mi servono per farlo funzionare
+const http = require('http');
 const path = require('path');
-app.use("/", express.static(path.join(__dirname, "public")));
-app.use("/files", express.static(path.join(__dirname, "files")));
-
-//libreria mysql2
-const fs = require('fs');
-const mysql = require('mysql2');
-const conf = JSON.parse(fs.readFileSync('./public/conf.json'));
-conf.ssl.ca = fs.readFileSync(__dirname + '/ca.pem');
-const connection = mysql.createConnection(conf);
-
-var storage = multer.diskStorage({
+const app = express();
+const multer  = require('multer');
+const database = require("./database");//importo database 
+database.createTable();
+const storage = multer.diskStorage({
     destination: function (req, file, callback) {
-        callback(null, path.join(__dirname, "/files"));
+        callback(null, path.join(__dirname, "files"));
     },
     filename: function (req, file, callback) {
         callback(null, file.originalname);
     }
 });
+const upload = multer({ storage: storage}).single('file');
 
-const executeQuery = (sql) => {
-    return new Promise((resolve, reject) => {
-        connection.query(sql, function (err, result) {
-            if (err) {
-                console.error(err);
-                reject();     
-            }   
-            console.log('done');
-            resolve(result);         
-        });
-    })
-}
+app.use("/", express.static(path.join(__dirname, "public")));//uso la cartella public
 
-const createTable = () => {
-    return executeQuery(`
-    CREATE TABLE IF NOT EXISTS immagini
-        ( id INT PRIMARY KEY AUTO_INCREMENT, 
-          url VARCHAR(255) NOT NULL ) 
-       `);      
-}
 
-const insert = (immagini) => {
-    const template = `
-    INSERT INTO immagini (url) VALUES ('$URL')
-       `;
-    let sql = template.replace("URL", immagini.url);
-    return executeQuery(sql); 
-}
+app.use("/files", express.static(path.join(__dirname, "files")));//uso la cartella file
 
-const select = () => {
-    const sql = `
-    SELECT id, url FROM immagini 
-       `;
-    return executeQuery(sql); 
-}
 
-createTable().then(() => {
-    insert({name: "test " + new Date().getTime(), completed: false}).then((result) => {
-        select().then(console.log);
-    });
+app.post("/upload", multer({ storage: storage}).single('file'), async (req, res) => {//per caricare foto
+  await database.insert("./files/" + req.file.originalname);
+  res.json({result: "ok" });  
 });
 
 
-let listaimmagini = [];
-
-app.post("/immagini/add", (req, res) => {
-   const immagini = req.body.immagini;
-   immagini.id = "" + new Date().getTime();
-   listaimmagini.push(immagini);
-   res.json({result: "Ok"});
+app.get('/images', async (req, res) => {//per mostrare le foto
+    //restituisce le immagini
+    const list = await database.select();
+    res.json(list);
 });
 
-app.get("/immagini", (req, res) => {
-   res.json({listaimmagini: listaimmagini});
-   console.log("entra dentro get")
-});
 
-app.delete("/immagini/:id", (req, res) => {
-    listaimmagini = listaimmagini.filter((element) => element.id !== req.params.id);
-    res.json({result: "Ok"});  
+app.delete('/delete/:id', async (req, res) => {//per cancellare le foto
+  //cancello in base a id
+  await database.delete(req.params.id);
+  res.json({result: "ok"});
 })
 
-const server = http.createServer(app);
 
-server.listen(5500, () => {
+const server = http.createServer(app);//creo il server
+server.listen(5600, () => {//avvio server alla porta 5600 di localhost
   console.log("- server running");
 });
