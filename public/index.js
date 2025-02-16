85f345165e9505a1c77d0cc80a96baf6432fa8e1
Alessandro Palmieri <palmierialessandro@itis-molinari.eu>
2025-02-16 15:53:14 +0100

    modifica file index con controller

--- public/index.js
+++ public/index.js
@@ -7,26 +7,26 @@ fetch("/conf.json")
           const navigator = createNavigator(document.querySelector("#container"));
       });
 
-(async () => {
-    const inputFile = document.querySelector('#file');
-    const button = document.querySelector("#button");
-    const link = document.querySelector("#link");
-  
-    const render = async () => {
-      const res = await fetch("/fileslist");
-      const data = await res.json();
-      link.innerHTML = data.map(file => `<li><a href="files/${file}">${file}</a></li>`).join("");
-    };
-  
-    (async () => {
-      await render();
-      })();
-  
-    const handleSubmit = async (event) => {
+
+
+const createMiddleware = () => {
+  return {
+    load: async () => {
+      const response = await fetch("/images");
+      const json = await response.json();
+      return json;
+    },
+    delete: async (id) => {
+      const response = await fetch("/delete/" + id, {
+        method: 'DELETE',
+      });
+      const json = await response.json();
+      return json;
+    },
+    upload: async (inputFile) => {
       const formData = new FormData();
       formData.append("file", inputFile.files[0]);
-      const body = formData;
-      body.description = inputFile.value;
+      const body = formData;      
       const fetchOptions = {
         method: 'post',
         body: body
@@ -34,13 +34,76 @@ fetch("/conf.json")
       try {
         const res = await fetch("/upload", fetchOptions);
         const data = await res.json();
-        link.setAttribute("href", data.url);
-        link.innerText = data.url;
-        await render();
+        console.log(data);
       } catch (e) {
         console.log(e);
       }
     }
+  }
+}
+
+const controller = async (middleware) => {  
+  const template = `
+    <tr>
+      <td><img src="$URL"style="width: 600px; height: 600px;" /></td>
+      <td><button id="$ID" type="button" class="delete btn btn-button" style="background-color:red">ELIMINA</button></td>
+    </tr>`;
+
+  const render = (list) => {
+    tabellaadmin.innerHTML = list.map((element) => {
+      let row = template.replace("$ID", element.id);
+      row = row.replace("$URL", element.name);
+      row = row.replace("$URL", element.name);
+      return row;
+    }).join("\n");
+    const bottoneelimina = document.querySelectorAll(".delete");
+    bottoneelimina.forEach((button) => {
+      button.onclick = () => {
+        middleware.delete(button.id)
+          .then(
+            () => middleware.load()
+          ).then((list) => {
+            render(list);
+            render1(list);
+          });
+      }
+    });
+  }
+
   
-    button.onclick = handleSubmit;
-  })();
+
+  const render1 = (list) => {
+    let html = '';
+    for (let i = 0; i < list.length; i++) {
+      const el = list[i];
+      let active = '';
+      if (i === 0) {
+        active = ' active';
+      }
+      html += '<div id="' + el.id + '" class="carousel-item' + active + '">'+
+       '<img src="' + el.name + '" style="width: 1000px; height: 600px;" alt="Immagine ' + el.id + '" />'+
+       '</div>';
+    }
+    carouselInner.innerHTML = html;
+  };
+
+  const inputFile = document.querySelector('#file');
+  const button = document.querySelector("#button");  
+  const tabellaadmin = document.getElementById("tabellaadmin");
+  const carouselInner = document.getElementById("carouselInner");
+
+  const handleSubmit = async (event) => {
+    await middleware.upload(inputFile);
+    const list = await middleware.load();
+    render1(list);
+    render(list);
+    inputFile.value=""
+  }
+  button.onclick = handleSubmit;
+  middleware.load().then((list) => {
+    render(list);
+    render1(list);
+  });
+}
+
+controller(createMiddleware());
